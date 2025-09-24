47d78783c2efae1d831be82d5457d6bd99c26272
Grigory Morozov <morozehc@yandex.ru>
2025-09-24 13:26:00 +0300

    refactor: remove useless log for it

--- src/ui/select/hooks/useOutsideClickClose.ts
+++ src/ui/select/hooks/useOutsideClickClose.ts
@@ -17,13 +17,15 @@ export const useOutsideClickClose = ({
 		// функция обработки клика вне селекта
 		const handleClick = (event: MouseEvent) => {
 			const { target } = event;
-			console.log('ИЗУЧИ!!!', target);
+			// 'ИЗУЧИ!!!'
+			// console.log('ИЗУЧИ!!!', target);
 			// клик вне корневого элемента селекта
 			// если таргет не экземпляр Node и корневой эелемент селекта его не содержит
 			//  вешается на корневой эелемент селекта чтобы проверить что селекта нет в корневом элементе
 			if (target instanceof Node && !rootRef.current?.contains(target)) {
 				isOpen && onClose?.(); // если isOpen true - список открыт, закрываем его
 				onChange?.(false); //и меняем состояние
+				// onClose? зачем
 			}
 		};
 		// навешиваем обработчик глобально
