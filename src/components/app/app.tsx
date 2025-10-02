3b081380f626697293ecf7f0008ddecef6392b6e
Grigory Morozov <morozehc@yandex.ru>
2025-10-02 17:50:55 +0300

    refactor: prepare App file apart from function rendeing components(form)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,5 @@
-import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState, Fragment } from 'react';
+import { CSSProperties, useState } from 'react';
 import { Article } from '../article';
-import { RadioGroup } from 'src/ui/radio-group';
-import { Separator } from 'src/ui/separator';
 import {
 	ArticleParamsForm,
 	submitForm,
@@ -13,14 +10,11 @@ import {
 	articlesData,
 } from 'src/constants/articleProps';
 import { FormElemsTitles } from 'src/constants/formData';
+import { setRenderElementsForm } from './AppSettings';
 import 'src/styles/index.scss';
 import styles from 'src/styles/index.module.scss';
-import { Select } from 'src/ui/select';
 
-const domNode = document.getElementById('root') as HTMLDivElement;
-const root = createRoot(domNode);
-
-const App = () => {
+export const App = () => {
 	const initStyle = {
 		'--font-family': defaultArticleState.fontFamilyOption.value,
 		'--font-size': defaultArticleState.fontSizeOption.value,
@@ -49,56 +43,9 @@ const App = () => {
 				titlesFields={FormElemsTitles}
 				onSubmit={(data) => applyStyles(data)}
 				onReset={() => applyStyles(initState)}>
-				{(renderElemData) => {
-					if (
-						renderElemData.key === 'fontSize' &&
-						renderElemData.title &&
-						renderElemData.selected
-					) {
-						return (
-							<RadioGroup
-								key={renderElemData.key}
-								title={renderElemData.title}
-								name={renderElemData.key}
-								options={renderElemData.options}
-								selected={renderElemData.selected}
-								onChange={renderElemData.onChange}
-							/>
-						);
-					}
-					//  добавляем разделитель
-					if (renderElemData.key === 'fontColor') {
-						return (
-							<Fragment key={renderElemData.key}>
-								<Select
-									options={renderElemData.options}
-									title={renderElemData.title}
-									selected={renderElemData.selected}
-									onChange={renderElemData.onChange}
-								/>
-								<Separator />
-							</Fragment>
-						);
-					}
-					// рендер по умолчанию
-					return (
-						<Select
-							key={renderElemData.key}
-							options={renderElemData.options}
-							title={renderElemData.title}
-							selected={renderElemData.selected}
-							onChange={renderElemData.onChange}
-						/>
-					);
-				}}
+				{(data) => setRenderElementsForm(data)}
 			</ArticleParamsForm>
 			<Article />
 		</main>
 	);
 };
-
-root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-);
